0c87e5b268e742bfd893fc935538288f917ab965
giroberto <umbertobarros@gmail.com>
2020-06-16 18:25:37 -0300

    Remove Preact Logo

--- src/app.jsx
+++ src/app.jsx
@@ -1,19 +1,11 @@
-import { Logo } from './logo'
+
 
 export function App(props) {
   return (
     <>
-      <Logo />
-      <p>Hello Vite + Preact!</p>
+      <p>umberto.dev</p>
       <p>
-        <a
-          class="link"
-          href="https://preactjs.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Preact
-        </a>
+        Software Developer
       </p>
     </>
   )
